b692e0beb5ab5152a332753e028c230a30149ada
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-08-16 18:12:37 -0300

    implementado componente TaskList

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,14 @@ import { FormCreateTask } from "./components/form-create-task/FormCreateTask";
 
 import { TaskType } from "./types/task.type";
 
+
+import clipboard from "./assets/images/clipboard.svg";
 import styles from "./App.module.css";
+import { TaskList } from "./components/task-list/TaskList";
 
 export function App() {
     const [tasks, setTasks] = useState([] as TaskType[]);
+    const isNoTasks = tasks.length === 0;
 
     function onCreateNewTask(title: string) {
         setTasks([...tasks, {
@@ -24,16 +28,23 @@ export function App() {
             <Header />
             <main className={styles.main}>
                 <FormCreateTask onCreateNewTask={onCreateNewTask} />
-                <ul>
-                    {tasks.map(task => {
-                        return (
-                            <li key={task.id}>
-                                {task.title}
-                            </li>
-                        )
-                    })}
-                </ul>
-            </main>
+                <article>
+                    <header></header>
+                    {
+                        isNoTasks ? (
+                            <div className={styles.noTasksBox}>
+                                <img src={clipboard} alt="Icone de uma prancheta com algumas coisas escritas" />
+                                <div className={styles.infoBox}>
+                                    <p>Você ainda não tem tarefas cadastradas</p>
+                                    <p>Crie tarefas e organize seus itens a fazer</p>
+                                </div>
+                            </div>
+                        ) : (
+                            <TaskList tasks={tasks} />
+                        )}
+                </article>
+
+            </main >
         </>
     )
 }
